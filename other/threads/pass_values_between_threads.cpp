a4c9a7b5fabee1066f6c76bc3010fe18dad74045
dgalus <dawid_galus@wp.pl>
2016-09-05 10:42:58 +0200

    pass values between threads

---
+++ other/threads/pass_values_between_threads.cpp
@@ -0,0 +1,16 @@
+#include <future>
+#include <thread>
+
+void func(std::promise<int> result_promise) noexcept
+{
+	result_promise.set_value(42);
+}
+
+int main()
+{
+	std::promise<int> result_promise;
+	std::future<int> result_future = result_future.get_future();
+	std::thread t{func, std::move(result_promise)};
+	int result = result_future.get();
+	t.join();
+}
